57bd9d38dcfd4e125c54177cb241af3aae898631
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-08-11 15:24:46 +0200

    Improved API to match design specs

--- routes/appointments.js
+++ routes/appointments.js
@@ -4,8 +4,8 @@ var router = express.Router();
 const appointments = [];
 
 router.post('/', function(req, res, next) {
-  if (!req.body.date || !req.body.slot) {
-    return res.status(400).send({error: "Object payload is malformed. Expected format is {date: 'yyyy-MM-dd', slot: 'HH:mm'}"});
+  if (!req.body.date || !req.body.slot || !req.body.orderConfirmationComment || !req.body.phone) {
+    return res.status(400).send({error: "Object payload is malformed. Expected format is {date: 'yyyy-MM-dd', slot: 'HH:mm', orderConfirmationComment: 'TEXT', phone: '+4901234567890'}"});
   }
 
   appointments.push(req.body);
@@ -15,7 +15,7 @@ router.post('/', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  res.status(201).send(appointments);
+  res.status(200).send(appointments);
 });
 
 module.exports = router;
--- routes/timeslots.js
+++ routes/timeslots.js
@@ -1,37 +1,68 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+
+const DATE_PATTERN = 'YYYY-MM-DD';
 
 router.get('/', function(req, res, next) {
-  res.status(200).send([
-    {
-      date: "2017-08-30", slots: [
-        {start: "08:00", end: "08:30"},
-        {start: "08:30", end: "09:00"},
-        {start: "09:00", end: "09:30"},
-        {start: "09:30", end: "10:00"},
-        {start: "11:00", end: "11:30"},
-        {start: "13:30", end: "14:00"},
-      ]
-    },
-    {
-      date: "2017-09-02", slots: [
-        {start: "08:45", end: "09:30"},
-        {start: "09:30", end: "10:15"},
-        {start: "10:15", end: "11:00"},
-        {start: "13:00", end: "13:45"},
-        {start: "15:00", end: "15:45"},
-      ]
-    },
-    {
-      date: "2017-09-05", slots: [
-        {start: "14:15", end: "14:30"},
-        {start: "14:30", end: "14:45"},
-        {start: "14:45", end: "15:00"},
-        {start: "18:00", end: "18:15"},
-        {start: "18:15", end: "18:30"},
-      ]
-    },
-  ]);
+
+  const today = new Date();
+  let slots = [];
+
+  slots.push({
+    date: moment().format(DATE_PATTERN), slots: [
+      {start: "08:00", end: "08:30"},
+      {start: "08:30", end: "09:00"},
+      {start: "09:00", end: "09:30"},
+      {start: "09:30", end: "10:00"},
+      {start: "11:00", end: "11:30"},
+      {start: "13:30", end: "14:00"},
+    ]
+  });
+
+  slots.push({
+    date: moment().add(1, 'days').format(DATE_PATTERN), slots: [
+      {start: "08:45", end: "09:30"},
+      {start: "09:30", end: "10:15"},
+      {start: "10:15", end: "11:00"},
+      {start: "13:00", end: "13:45"},
+      {start: "15:00", end: "15:45"},
+    ]
+  });
+
+  slots.push({
+    date: moment().add(2, 'days').format(DATE_PATTERN), slots: [
+      {start: "14:15", end: "14:30"},
+      {start: "14:30", end: "14:45"},
+      {start: "14:45", end: "15:00"},
+      {start: "18:00", end: "18:15"},
+      {start: "18:15", end: "18:30"},
+    ]
+  });
+
+  slots.push({
+    date: moment().add(3, 'days').format(DATE_PATTERN), slots: []
+  });
+
+  slots.push({
+    date: moment().add(4, 'days').format(DATE_PATTERN), slots: []
+  });
+
+  slots.push({
+    date: moment().add(5, 'days').format(DATE_PATTERN), slots: [
+      {start: "14:15", end: "14:30"},
+      {start: "14:30", end: "14:45"},
+      {start: "14:45", end: "15:00"},
+      {start: "18:00", end: "18:15"},
+      {start: "18:15", end: "18:30"},
+    ]
+  });
+
+  slots.push({
+    date: moment().add(6, 'days').format(DATE_PATTERN), slots: []
+  });
+
+  res.status(200).send(slots);
 });
 
 module.exports = router;
