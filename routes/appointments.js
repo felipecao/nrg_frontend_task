91aaaa60bf2656ad25fcf783c585d335746d08a9
Felipe Carvalho <felipe.carvalho@gmail.com>
2017-08-11 16:04:07 +0200

    Improve appointments resource to return a random stylist upon booking

--- routes/appointments.js
+++ routes/appointments.js
@@ -2,16 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 const appointments = [];
+const stylists = ['Julia', 'Ligia', 'Alex', 'Felipe', 'Daniela', 'Sebastian', 'Shaun', 'Dmitrii', 'Antonella'];
+
+function randomStylist() {
+  const randomIndex = Math.floor(Math.random() * stylists.length);
+
+  return stylists[randomIndex];
+}
 
 router.post('/', function(req, res, next) {
   if (!req.body.date || !req.body.slot || !req.body.orderConfirmationComment || !req.body.phone) {
     return res.status(400).send({error: "Object payload is malformed. Expected format is {date: 'yyyy-MM-dd', slot: 'HH:mm', orderConfirmationComment: 'TEXT', phone: '+4901234567890'}"});
   }
 
-  appointments.push(req.body);
+  const confirmation = Object.assign({stylist: randomStylist()}, req.body);
 
-  console.log(`A new appointment has been created: ${JSON.stringify(req.body)}`);
-  res.status(201).send();
+  appointments.push(confirmation);
+  console.log(`A new appointment has been created: ${JSON.stringify(confirmation)}`);
+  
+  res.status(201).send(confirmation);
 });
 
 router.get('/', function(req, res, next) {
